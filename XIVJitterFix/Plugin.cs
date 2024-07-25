0b2c4ead66501243f0656f281246e78fdb4edcbb
HoloWise <tomekrachanski38@gmail.com>
2024-07-25 11:31:25 +0200

    Adds basic command functionality

--- XIVJitterFix/Plugin.cs
+++ XIVJitterFix/Plugin.cs
@@ -3,6 +3,8 @@ using Dalamud.Plugin.Services;
 using Dalamud.Game;
 using System.Runtime.InteropServices;
 using Dalamud.Interface.Windowing;
+using Dalamud.Game.Command;
+using System;
 
 namespace XIVJitterFix;
 
@@ -14,15 +16,17 @@ public sealed class Plugin : IDalamudPlugin
     private readonly IFramework framework;
     private readonly WindowSystem windowSystem;
     private readonly IDalamudPluginInterface dalamudPluginInterface;
+    private readonly ICommandManager commandManager;
     private readonly MainWindow mainWindow;
     private readonly Config pluginConfig;
 
     public unsafe Plugin(IPluginLog logger, ISigScanner sigScanner,
-        IFramework framework, IDalamudPluginInterface dalamudPluginInterface)
+        IFramework framework, IDalamudPluginInterface dalamudPluginInterface, ICommandManager commandManager)
     {
         this.logger = logger;
         this.framework = framework;
         this.dalamudPluginInterface = dalamudPluginInterface;
+        this.commandManager = commandManager;
         windowSystem = new("XIVJitterFix");
         pluginConfig = dalamudPluginInterface.GetPluginConfig() as Config ?? new();
 
@@ -41,11 +45,24 @@ public sealed class Plugin : IDalamudPlugin
         mainWindow = new MainWindow(pluginConfig, dalamudPluginInterface);
         windowSystem.AddWindow(mainWindow);
 
+        commandManager.AddHandler("/jitterfix", new CommandInfo(OnCommand) { HelpMessage = $"Open the XIVJitterFix config window.", ShowInHelp = true });
+
         hookAddr = sigScanner.GetStaticAddressFromSig("48 89 05 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8D 8F ?? ?? ?? ??");
 
         framework.Update += Framework_Update;
         dalamudPluginInterface.UiBuilder.OpenConfigUi += UiBuilder_OpenConfigUi;
         dalamudPluginInterface.UiBuilder.Draw += UiBuilder_Draw;
+    }
+
+
+    private void OnCommand(string command, string args)
+    {
+        var splitArgs = args.ToLowerInvariant().Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries); //Setting specific commands?
+
+        if (splitArgs.Length == 0)
+        {
+            mainWindow.Toggle();
+        }
     }
 
     private void UiBuilder_Draw()
@@ -93,6 +110,7 @@ public sealed class Plugin : IDalamudPlugin
 
     public void Dispose()
     {
+        commandManager.RemoveHandler("/jitterfix");
         windowSystem.RemoveAllWindows();
 
         dalamudPluginInterface.UiBuilder.OpenConfigUi -= UiBuilder_OpenConfigUi;
